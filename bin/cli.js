63693b0d9984ba1a7cce4f6105ce1f2bfe2e8a88
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-03-04 21:36:46 +0530

    Refactor project structure: remove index.js, update main entry in package.json, and enhance post-install script with welcome message

--- bin/cli.js
+++ bin/cli.js
@@ -1,13 +1,15 @@
-import { GetComponent } from "../src/script.js";
+#!/usr/bin/env node
 
+import { GetComponent } from "../src/script.js";
 const args = process.argv.slice(2);
 
 const handleComponent = async (componentName) => {
     try {
         await GetComponent(componentName);
-        console.log(`\x1b[34mComponent ${componentName} added successfully!`);
+        console.log(`\x1b[34mComponent ${componentName} added to utils folder successfully!`);
     } catch (error) {
         console.error('\x1b[31m\x1b[1mError fetching component\x1b[37m');
+        console.error(error);
     }
 }
 
@@ -15,7 +17,7 @@ if (args.length < 2 || args[0] !== "add") {
     console.error("Usage: npx trydivjs add <component_name>");
     process.exit(1);
 }
-else{
+else {
     const componentName = args[1];
     console.log(`Creating component ${componentName}...`);
     handleComponent(componentName);
